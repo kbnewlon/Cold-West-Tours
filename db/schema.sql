326b84c5e198e9adf71ff6daeaaa4cd332284349
Kayla Newlon <kbnewlon@hotmail.com>
2020-10-20 11:09:32 -0700

    adding seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,16 @@ CREATE DATABASE winter_activity_db;
 
 USE winter_activity_db;
 
-CREATE TABLE `resort`
+CREATE TABLE `resorts`
 (
     id int NOT NULL,
     name VARCHAR(255) NOT NULL,
     address VARCHAR (255) NOT NULL,
     phone VARCHAR(15) NOT NULL,
-    address VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
 );
 
-CREATE TABLE `activity`
+CREATE TABLE `activities`
 (
      id int NOT NULL,
      name VARCHAR (255) NOT NULL, 

6bfe05e6a446f6a41fcb5edca8a5e84a0493bdf5
Chris MacGeorge <christopher.s.macgeorge@gmail.com>
2020-10-20 11:50:17 -0700

    activity seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,4 +9,16 @@ INSERT INTO resorts (name, address, phone) VALUES ()
 
 
 
-INSERT INTO activities (name, slope) VALUES ()
+
+
+
+
+
+
+INSERT INTO activities (name, slope) VALUES (skiing, true)
+INSERT INTO activities (name, slope) VALUES (cross country skiing, false)
+INSERT INTO activities (name, slope) VALUES (snow boarding, true)
+INSERT INTO activities (name, slope) VALUES (snow shoeing, false)
+INSERT INTO activities (name, slope) VALUES (snowmobiling, false)
+INSERT INTO activities (name, slope) VALUES (tubing, true)
+INSERT INTO activities (name, slope) VALUES (dining, false)

de922ed98d9b3a76d6a1cf831de36a4770e4eb13
Artemis <wzyzhouyi@gmail.com>
2020-10-26 14:56:14 -0700

    changed dropdown setting

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -175,7 +175,7 @@ $('.dropdown-trigger').dropdown({
     inDuration: 300,
     outDuration: 225,
     constrain_width: false, // Does not change width of dropdown to that of the activator
-    hover: true, // Activate on hover
+    hover: false, // Activate on hover
     constrainWidth: false, //changed width of dropdown due to content size 
     gutter: 0, // Spacing from edge
     belowOrigin: false, // Displays dropdown below the button
